9f14113cfd8d21df54a8738091c556b37fd22988
PAT <patit_rsu@hotmail.com>
2016-12-26 11:36:16 +0700

    no message

---
+++ verify.php
@@ -0,0 +1,17 @@
+<?php
+$access_token = 'RZ1r4Q9oSx8PfqfpR+ZPMaiRUg9IZesij7t6Vz10LPO4ODUyJJ+a/I/6l9wx/aYb8xZFeY8THKZpv68/u3rEKaiQ/LHuzaUP4iBRqqs/2bVAowFli+8pwR/bMzudjr+UotuGXVZgEh0F0T7qjO3obAdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+?>
