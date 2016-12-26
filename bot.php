b520a3bba342d3c9efec4085554475378055578e
PAT <patit_rsu@hotmail.com>
2016-12-26 12:02:46 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,8 +1,9 @@
 <?php
 $access_token = 'RZ1r4Q9oSx8PfqfpR+ZPMaiRUg9IZesij7t6Vz10LPO4ODUyJJ+a/I/6l9wx/aYb8xZFeY8THKZpv68/u3rEKaiQ/LHuzaUP4iBRqqs/2bVAowFli+8pwR/bMzudjr+UotuGXVZgEh0F0T7qjO3obAdB04t89/1O/w1cDnyilFU=';
-
+$path = "/D:/Line_Group/LINE-BOT-PHP-Starter/myFiie.json"
+echo $path;
 // Get POST body content
-$content = file_get_contents('D:\Line_Group\LINE-BOT-PHP-Starter\myFiie.json');
+$content = file_get_contents("path");
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
