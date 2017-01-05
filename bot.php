662bd064a3b02a887e0fe89f9211a9f7eca83b43
pat <patit_rsu@hotmail.com>
2017-01-05 16:41:40 +0700

    no message

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@ $access_token = '5gU8HodUfwdJ7JbvJMYoE5I2CSYdl5G8cB8mfmUXmN4CySTBlf5SkRQtnS54h8h
 $proxy = 'http://fixie:XSPmbLgrvhlfM9U@velodrome.usefixie.com:80';
 $proxyauth = 'velodrome:XSPmbLgrvhlfM9U';
 // Get POST body content
-$content = {"to":"ubef4da75535c95f60a23379739fc3221","messages":{"type":"text","text":"HelloWorld"}};
+$content = {"to":"ubef4da75535c95f60a23379739fc3221","messages":[{"type":"text","text":"HelloWorld"}]};
 $content = file_get_contents('https://github.com/patitrsu/LINE-BOT-PHP-Starter/blob/master/myFile.Json');
 // Parse JSON
 $events = json_decode($content, true);
