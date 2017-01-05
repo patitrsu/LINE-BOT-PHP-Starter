94774e5a08596ffc1e37a1ce2abb61ae444793b0
pat <patit_rsu@hotmail.com>
2017-01-05 22:49:18 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,14 +1,12 @@
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 $access_token = '5gU8HodUfwdJ7JbvJMYoE5I2CSYdl5G8cB8mfmUXmN4CySTBlf5SkRQtnS54h8h78xZFeY8THKZpv68/u3rEKaiQ/LHuzaUP4iBRqqs/2bUzqFjwQIxNONJ2BpEy2eVNwmeoNadkH7MLzX7aHcAkIAdB04t89/1O/w1cDnyilFU=';
 $proxy = 'http://fixie:XSPmbLgrvhlfM9U@velodrome.usefixie.com:80';
 $proxyauth = 'velodrome:XSPmbLgrvhlfM9U';
 // Get POST body content
-$json = "{"to":"ubef4da75535c95f60a23379739fc3221","message":[{"type":"text"},{"text":"Helloworld"}] }";
+$json = "{"to":"ubef4da75535c95f60a23379739fc3221" , "message":[{"type":"text","text":"helloworld"}]}";
 
 //$content = file_get_contents('php://input');
 // Parse JSON
@@ -37,8 +35,7 @@ $json = "{"to":"ubef4da75535c95f60a23379739fc3221","message":[{"type":"text"},{"
         'messages' => [$messages],
       ];*/
       $post = json_encode($json);
-      echo $post ;
-  /*    $url = 'https://api.line.me/v2/bot/message/push';
+      $url = 'https://api.line.me/v2/bot/message/push';
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
       $ch = curl_init($url);
@@ -52,7 +49,7 @@ $json = "{"to":"ubef4da75535c95f60a23379739fc3221","message":[{"type":"text"},{"
       $result = curl_exec($ch);
       curl_close($ch);
 
-      echo $result . "\r\n";*/
+      echo $result . "\r\n";
     }
   }
 }
