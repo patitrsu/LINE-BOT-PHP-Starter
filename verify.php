ecc627cc9554593de9139a4f6cccbcf387e0285b
pat <patit_rsu@hotmail.com>
2017-01-05 15:12:33 +0700

    no message

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'http://fixie:XSPmbLgrvhlfM9U@velodrome.usefixie.com:80';
+$proxyauth = 'velodrome:XSPmbLgrvhlfM9U';
+
 $access_token = '5gU8HodUfwdJ7JbvJMYoE5I2CSYdl5G8cB8mfmUXmN4CySTBlf5SkRQtnS54h8h78xZFeY8THKZpv68/u3rEKaiQ/LHuzaUP4iBRqqs/2bUzqFjwQIxNONJ2BpEy2eVNwmeoNadkH7MLzX7aHcAkIAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -8,9 +11,12 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
 
 ?>
+
