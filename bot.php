0d9213f615b390554cfd62fe35d301060e7d8960
pat <patit_rsu@hotmail.com>
2017-01-09 09:29:29 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,4 +1,3 @@
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -37,22 +36,23 @@ if (!is_null($events['events'])) {
       ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+      echo json_decode($data);
 
-       $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($ch, CURLOPT_PROXY, $proxy);
-      curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-      $result = curl_exec($ch);
-      curl_close($ch);
+    //  $ch = curl_init($url);
+    //  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    //  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    //  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    //  curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    //  curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+    //  $result = curl_exec($ch);
+    //  curl_close($ch);
 
-      echo $post . "\r\n";
+    //  echo $result . "\r\n";
 
     }
   }
 }
-echo "OK";
+//echo "OK";
 ?>
