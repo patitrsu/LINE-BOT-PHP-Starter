d13567db53d6141622c47f6275506b695880cbfe
pat <patit_rsu@hotmail.com>
2017-01-09 11:31:30 +0700

    no message

--- bot.php
+++ bot.php
@@ -22,10 +22,12 @@ if (!is_null($events['events'])) {
       // Get replyToken
       $replyToken = $event['replyToken'];
 
+      $user = $event['source']['userId']
+
       // Build message to reply back
       $messages = [
         'type' => 'text',
-        'text' => $text
+        'text' => $user
       ];
 
       // Make a POST Request to Messaging API to reply to sender
