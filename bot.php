4ddaec685511075d7f06dc4701653cfcc3ee2746
pat <patit_rsu@hotmail.com>
2017-01-09 11:27:49 +0700

    no message

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
   // Loop through each event
   foreach ($events['events'] as $event) {
     // Reply only when message sent is in 'text' format
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text'&& $event['source'] == 'user')      
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text')      
       // Get text sent
       $text = $event['message']['text'];
       // GET USER Sent
